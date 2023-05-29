edbea3b3b146af0112d6b4031b85547e79989c90
NymWidana <widan0001@gmail.com>
2023-05-29 22:19:55 +0800

    update3

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,31 +1,40 @@
-<script>
-</script>
+<script></script>
 <template>
-    <div class="layoutcenter">
-        <!-- tentangkami -->
-          <div class="mb-8">
-        <h2 class="text-center text-3xl font-black py-8">Tentang Kami</h2>
-        <div class="grid grid-cols-2">
-          <h3 class="flex items-center text-3xl font-black px-4 py-8 underline">
-            MaiKubu Tigawasa
-          </h3>
-          <div class="bg-color1 text-white px-4 h-96 flex items-center">
-            MaiKubu terdiri dari dua kata "Mai" dan "Kubu". Dua kata ini kami
-            ambil dari kalimat yang sering kami utarakan ketika ingin mengajak
-            teman untuk pulang. Melalui nama ini kami ingin berpesan untuk
-            selalu ingat kepada tanah kelahiran.
-          </div>
-        </div>
-        <div class="grid grid-cols-2">
-          <div class="bg-color3 text-white px-4 h-96 flex items-center">
-            Tigawasa kami ambil dari nama desa tempat kami lahir, yakni Desa
-            Tigawasa. Sebuah desa tua di Bali Utara yang kaya akan nilai hidup
-            selaras alam dari adat dan budaya yang dijalankan.
-          </div>
-          <h3 class="flex items-center text-3xl font-black px-4 py-8 underline">
-            MaiKubu-Ayo ke Rumah / Ayo Pulang
-          </h3>
+  <div class="layoutcenter">
+    <!-- tentang kami -->
+    <div class="mb-8">
+      <h2 class="text-center text-3xl font-black py-8">Tentang Kami</h2>
+      <div class="grid sm:grid-cols-2 grid-cols-1">
+        <h3
+          class="flex items-center text-3xl font-black px-4 py-8 underline sm:bg-white bg-gray-100"
+        >
+          MaiKubu Tigawasa
+        </h3>
+        <div
+          class="bg-color1 text-white px-4 py-4 pb-8 sm:h-96 h-auto flex items-center"
+        >
+          <p>
+            <b class="font-black">MaiKubu</b> terdiri dari dua kata "Mai" dan
+            "Kubu". Dua kata ini kami ambil dari kalimat yang sering kami
+            utarakan ketika ingin mengajak teman untuk pulang. Melalui nama ini
+            kami ingin berpesan untuk selalu ingat kepada tanah kelahiran.
+          </p>
         </div>
       </div>
-</div>
+      <div class="flex flex-wrap">
+        <div
+          class="sm:w-1/2 w-full bg-color3 text-white px-4 py-4 pb-8 sm:h-96 h-auto flex items-center"
+        >
+          Tigawasa kami ambil dari nama desa tempat kami lahir, yakni Desa
+          Tigawasa. Sebuah desa tua di Bali Utara yang kaya akan nilai hidup
+          selaras alam dari adat dan budaya yang dijalankan.
+        </div>
+        <h3
+          class="sm:order-1 -order-1 sm:w-1/2 w-full flex items-center text-3xl font-black px-4 py-8 underline sm:bg-white bg-gray-100"
+        >
+          MaiKubu-Ayo ke Rumah / Ayo Pulang
+        </h3>
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/Workshop.vue
+++ src/components/Workshop.vue
@@ -1,36 +1,36 @@
 <template>
-    <div class="layoutcenter">
-          <!-- kegiatan maikubu -->
-      <div class="my-8" id="workshop">
-        <h2 class="text-center text-3xl font-black py-8">Workshop Kami</h2>
-        <div class="grid md:grid-cols-2 grid-cols-1 mb-12">
-          <img class="rounded-lg" src="../assets/kegiatan1.png" alt="" />
-          <div class="p-4">
-            <h3 class="text-3xl font-black underline">Kegiatan 1</h3>
-            <p class="my-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-              fuga, ut voluptatem placeat excepturi ipsam fugit. Officiis quo
-              quasi consequuntur optio, corrupti iste odio beatae recusandae
-              soluta assumenda eveniet animi, molestias asperiores itaque quis
-              magni fugiat saepe illo aliquam in. Reprehenderit, deleniti
-              maxime. Ut, iste hic porro id adipisci dolor obcaecati? Veniam
-              amet, explicabo corporis sit atque voluptatem magni in!
-            </p>
-          </div>
-        </div>
-        <div class="">
-          <img class="rounded-lg" src="../assets/kegiatan2.png" alt="" />
-          <div class="p-4">
-            <h3 class="text-3xl font-black underline">Kegiatan 2</h3>
-            <p class="my-4">
-              Kami lahir sebagai upaya pelestarian adat dan budaya Desa Tigawasa
-              yang sangat erat kaitannya dengan nilai konservasi. Kami ingin
-              menciptakan ekonomi yang berdaya dan berkeadilan dari kegiatan
-              masyarakat dalam keseharian, salah satunya melalui budaya
-              menganyam bambu.
-            </p>
-          </div>
+  <div class="layoutcenter">
+    <!-- workshop maikubu -->
+    <div class="my-8" id="workshop">
+      <h2 class="text-center text-3xl font-black py-8">Workshop Kami</h2>
+      <div class="grid md:grid-cols-2 grid-cols-1 mb-12">
+        <img class="rounded-lg m-auto" src="../assets/kegiatan1.png" alt="" />
+        <div class="p-4">
+          <h3 class="text-3xl font-black underline">Kegiatan 1</h3>
+          <p class="my-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            fuga, ut voluptatem placeat excepturi ipsam fugit. Officiis quo
+            quasi consequuntur optio, corrupti iste odio beatae recusandae
+            soluta assumenda eveniet animi, molestias asperiores itaque quis
+            magni fugiat saepe illo aliquam in. Reprehenderit, deleniti maxime.
+            Ut, iste hic porro id adipisci dolor obcaecati? Veniam amet,
+            explicabo corporis sit atque voluptatem magni in!
+          </p>
         </div>
       </div>
-</div>
+      <div class="">
+        <img class="rounded-lg" src="../assets/kegiatan2.png" alt="" />
+        <div class="p-4">
+          <h3 class="text-3xl font-black underline">Kegiatan 2</h3>
+          <p class="my-4">
+            Kami lahir sebagai upaya pelestarian adat dan budaya Desa Tigawasa
+            yang sangat erat kaitannya dengan nilai konservasi. Kami ingin
+            menciptakan ekonomi yang berdaya dan berkeadilan dari kegiatan
+            masyarakat dalam keseharian, salah satunya melalui budaya menganyam
+            bambu.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
